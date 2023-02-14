dfd2cc954e815d4328fb455d92b2f5e2cbb549f0
Neyzer538 <neyzergomez1002@gmail.com>
2023-02-14 10:08:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print "Neyzer Paul Gómez López 1651623";
+print "Neyzer Paul Gómez López 1651623"
+import os
+
+mi ubicación = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi ubicación + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
